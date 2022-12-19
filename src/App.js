b865b3234cd00d1aa4bce62a38f78bb8663beaa2
Eric Chi <ericchi00@gmail.com>
2022-12-19 13:07:44 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Photo from "./Components/Photo";
+
+const App = () => {
+  return (
+    <div className="container-fluid">
+      <div className="d-flex justify-content-center p-2 flex-column align-items-center">
+        <p className="fs-1">Cat Breed Predictor</p>
+        <p className="fs-6">
+          Cat URL must be in .jpg format to work. For more accurate results, the
+          cat must be at least 80% focus of the photo with the entire body in
+          the photo.
+        </p>
+      </div>
+      <Photo />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Photo.js
@@ -0,0 +1,208 @@
+import React, { useEffect, useState } from "react";
+import "chart.js/auto";
+import { Pie } from "react-chartjs-2";
+import TrainingAccuracy from "../Assets/Figure_1.png";
+
+const Photo = () => {
+  const [cat, setCat] = useState();
+  const [confidenceLevel, setConfidenceLevel] = useState();
+  const [catImage, setCatImage] = useState(null);
+  const [haveData, setHaveData] = useState(false);
+  const [data, setData] = useState();
+  const [filteredData, setFilteredData] = useState({});
+  const [link, setLink] = useState();
+
+  const classNames = [
+    "Abyssinian",
+    "American Shorthair",
+    "Bengal",
+    "Birman",
+    "Bombay",
+    "British Shorthair",
+    "Egyptian Mau",
+    "Maine Coon",
+    "Persian",
+    "Ragdoll",
+    "Russian Blue",
+    "Scottish Fold",
+    "Siamese",
+    "Sphynx",
+  ];
+
+  useEffect(() => {
+    getRandomImage();
+  }, []);
+
+  const getRandomImage = async () => {
+    const imageAPI = await fetch(
+      "https://api.thecatapi.com/v1/images/search?api_key=live_OqPa5Sbstd2cU7uq9BkMTmwvV6uCKxuGcekfuRHjqAtLHlMqGYhCYQFLAOyGyoH3&mime_types=jpg"
+    );
+    const response = await imageAPI.json();
+    setCatImage(response[0].url);
+    setCatBreedProbability(response[0].url);
+  };
+
+  const setCatBreedProbability = async (link) => {
+    const headers = {
+      mode: "cors",
+      method: "POST",
+      url: "https://catbreedpredictorapi.herokuapp.com/",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: link,
+      }),
+    };
+    const url = await fetch(
+      `https://catbreedpredictorapi.herokuapp.com/`,
+      headers
+    );
+    const response = await url.json();
+    setCat(classNames[response[0]]);
+    const data = convertArrayToNumber(response);
+    const filteredData = [];
+    const filteredLabels = [];
+    for (let i = 0; i < classNames.length; i++) {
+      if (data[i] !== 0) {
+        filteredLabels.push(classNames[i]);
+        filteredData.push(data[i]);
+      }
+    }
+    setData(data);
+    setConfidenceLevel(getMaxNumFromArray(filteredData));
+    setFilteredData({
+      labels: filteredLabels,
+      datasets: [
+        {
+          label: "Cat Breed Probability",
+          data: filteredData,
+          backgroundColor: [
+            "#0074D9",
+            "#FF4136",
+            "#2ECC40",
+            "#FF851B",
+            "#7FDBFF",
+            "#B10DC9",
+            "#FFDC00",
+            "#001f3f",
+            "#39CCCC",
+            "#01FF70",
+            "#85144b",
+            "#F012BE",
+            "#3D9970",
+            "#111111",
+            "#AAAAAA",
+          ],
+        },
+      ],
+    });
+    setHaveData(true);
+  };
+
+  const getMaxNumFromArray = (arr) => {
+    let max = 0;
+    for (let i = 0; i < arr.length; i++) {
+      max = Math.max(max, arr[i]);
+    }
+    return max;
+  };
+
+  const submitURL = () => {
+    setCatImage(link);
+    setCatBreedProbability(link);
+    setHaveData(true);
+  };
+
+  const convertArrayToNumber = (arr) => {
+    arr.splice(0, 1);
+    const data = arr;
+    for (let i = 0; i < data.length; i++) {
+      data[i] = Math.round(Number(data[i]).toFixed(20) * 100);
+    }
+    return data;
+  };
+
+  return (
+    <div className="container d-flex justify-content-center flex-column">
+      <div className="d-flex justify-content-center mb-4 gap-4 align-items-center">
+        <button
+          type="button"
+          className="btn btn-primary btn-lg"
+          onClick={() => {
+            getRandomImage();
+            setHaveData(!haveData);
+          }}
+        >
+          Click to get a random cat image!
+        </button>
+        <div className="input-group-lg">
+          <input
+            className="form-control"
+            id="catImageLink"
+            type="text"
+            placeholder="Put cat image URL link here"
+            onChange={(e) => {
+              setLink(e.target.value);
+            }}
+          />
+        </div>
+        <button
+          type="button"
+          className="btn btn-secondary btn-lg"
+          onClick={() => {
+            submitURL();
+            setHaveData(!haveData);
+          }}
+        >
+          Submit cat link!
+        </button>
+      </div>
+      <div className="container-fluid d-flex align-items-center justify-content-center gap-2 flex-column mb-5">
+        {haveData === false ? (
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        ) : (
+          <>
+            <p className="fs-5">
+              This cat is most likely a <strong>{cat}</strong> with a{" "}
+              <strong>{confidenceLevel}</strong> percent confidence.
+            </p>
+            <div className="d-flex align-items-center">
+              <img
+                src={catImage}
+                className="rounded img-fluid"
+                alt="cat"
+                style={{ maxHeight: "600px", maxWidth: "600px" }}
+              />
+              <Pie
+                data={filteredData}
+                options={{
+                  animation: false,
+                }}
+                style={{ maxHeight: "400px", maxWidth: "400px" }}
+              />
+              <ul class="list-group list-group-numbered">
+                {classNames.map((label, i) => {
+                  return (
+                    <li className="list-group-item" key={i}>
+                      {label}: {data[i]}%
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </>
+        )}
+        {haveData === false ? (
+          <></>
+        ) : (
+          <img src={TrainingAccuracy} alt="graph of training accuracy" />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Photo;
